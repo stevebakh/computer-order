e5e5ea1f39adc07dfdbaf180c67aaf37453a48ab
Steven Bakhtiari <steven@substrate.co.uk>
2020-04-11 13:17:11 +0100

    Update ComputerOrder.java

--- src/main/java/com/mycompany/computerorder/ComputerOrder.java
+++ src/main/java/com/mycompany/computerorder/ComputerOrder.java
@@ -11,82 +11,28 @@ import java.util.Scanner;
  * @author maria
  */
 public class ComputerOrder {
-    public static void main (String[] args )
-    {
-        double size, optional;
-        final double computer = 375.99;
-        Scanner keyboard = new Scanner (System.in);
-        System.out.println("COMPUTER ORDER"); 
-        System.out.println("Basic model cost 375.99");
-        System.out.println("Select screen size (1 for screen38 or 2 for screen43)");
-        size = keyboard.nextDouble();
-        if (size == 1)
-        {
-            System.out.println("Price = " + (computer + 75.99) );
-        }
-        if (size == 2)
-        {
-            System.out.println("Price = " + (computer + 99.99) );
-        }
-        System.out.println("You can select optional items: antivirus, printer");
-        System.out.println("Select 1 for antivirus, 2 for printer, 3 for both or 0 for No, thank you");
-        optional = keyboard.nextDouble();
-        if (optional == 1 && size == 1)
-        {
-            System.out.println("Price TOTAL = " + (computer + (75.99 + 65.99)));
-        }
-        if (optional == 1 && size == 2)
-        {
-            System.out.println("Price TOTAL = " + (computer + (99.99 +65.99)));
-        }
-        if (optional == 2 && size == 1)
-        {
-            System.out.println("Price TOTAL = " + (computer + (75.99 + 125)));
-        }
-        if (optional == 2 && size == 2)
-        {
-            System.out.println("Price TOTAL = " + (computer + (99.99 + 125)));
-        }
-        if (optional == 3 && size == 1)
-        {
-            System.out.println("Price TOTAL = " + (computer + (75.99 + 65.99 + 125)));
-        }
-        if (optional == 3 && size == 2)
-        {
-            System.out.println("Price TOTAL = " + (computer + (99.99 + 65.99 + 125)));
-        }
-        if (optional == 0 && size == 1)
-        {
-            System.out.println("Price TOTAL = " + (computer + 75.99));
-        }
-        if (optional == 0 && size == 2)
-        {
-            System.out.println("Price TOTAL = " + (computer + 99.99));
-        }
-            
-        }
-        
-        
-                    
-                }
-        
-        
-        
-           
-        
-        
-        
-        
-        
-              
-     
-      
-        
-        
-        
-                
-                
-                
-    
-    
 
+    private static final double BASIC_PRICE = 375.99;
+
+    public static void main(String[] args) {
+        System.out.println("COMPUTER ORDER");
+        Scanner keyboard = new Scanner(System.in);
+
+        double total = BASIC_PRICE;
+
+        System.out.print("Select screen size. [1] 38cm,  [2] 43cm: ");
+        if (keyboard.nextInt() == 1) {
+            total = total + 75.99;
+        } else if (keyboard.nextInt() == 2) {
+            total = total + 99.99;
+        }
+
+        System.out.print("Include Antivirus software? [Y/n]: ");
+        if (keyboard.next().toUpperCase().equals("Y")) total = total + 65.99;
+
+        System.out.print("Include printer? [Y/n]: ");
+        if (keyboard.next().toUpperCase().equals("Y")) total = total + 125.00;
+
+        System.out.println("Total system price: " + total);
+    }
+}
